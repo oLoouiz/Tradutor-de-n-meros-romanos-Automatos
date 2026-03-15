56873c52b23db7f1ebf2915bfcb903536fef87b8
Raphael Shodi <80370866+kosh202@users.noreply.github.com>
2026-03-14 23:13:58 -0300

    primeira parte

--- afn-celso.rb
+++ afn-celso.rb
@@ -19,27 +19,51 @@ class ADF
   
   def iniciar
     estado = "q0"
-    saida = ""
+    saida = 0
   
     puts "Máquina iniciou no estado: " + estado
   
     loop do
       case [proximo, estado]
-      in ["a", "q0"]
-        estado = "q0"
-        saida += "b"
-      in ["b", "q0"]
+      in ["I", "q0"]
         estado = "q1"
-        saida += "a"
-      in ["b", "q1"]
+        saida += 1
+	puts "Saída: #{saida}"
+      in ["I", "q1"]
         estado = "q2"
-        saida += "a"
-      in ["", "q2"]
-        saida += ""
+        saida += 1
+	puts "Saída: #{saida}"
+      in ["X", "q1"]
+        estado = "q6"
+        saida += 8
+	puts "Saída: #{saida}"
+      in ["V", "q1"]
+	estado = "q5"
+        saida += 3
+	puts "Saída: #{saida}"
+	in ["I", "q2"]
+	  estado= "q3"	
+	  saida += 1
+	  puts "Saída: #{saida}"
+	in ["V", "q0"]
+	  estado = "q7"
+	  saida += 5
+	  puts "Saída: #{saida}"
+	in ["I", "q7"]
+	  estado = "q8"
+	  saida += 1
+	  puts "Saída: #{saida}"
+	in ["I", "q8"]
+	  estado = "q9"
+	  saida +=1
+	  puts "Saída: #{saida}"
+	in ["I", "q9"]
+	  estado ="q10"
+	  saida+= 1
         puts "Aceito 😀"
         puts "Estado final: " + estado
         puts "Entrada: " + @cadeia
-        puts "Saída: " + saida
+        puts "Saída: #{saida}"
         break
       else
         puts "Erro"
@@ -52,6 +76,6 @@ class ADF
   end
 end
 
-# entrada = "aabb"
-adf = ADF.new("aabb")
+# entrada = "VII"
+adf = ADF.new("VII")
 adf.iniciar
